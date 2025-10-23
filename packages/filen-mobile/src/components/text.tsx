8d986784348412f68081185c4c1762275ae16f79
Dwynr <dwynr@outlook.de>
2025-10-24 01:44:29 +0200

    feat(mobile): uniwind, boost

---
+++ packages/filen-mobile/src/components/text.tsx
@@ -0,0 +1,6 @@
+import { NativeText } from "react-native-boost/runtime"
+import { withUniwind } from "uniwind"
+
+export const Text = withUniwind(NativeText)
+
+export default Text
---
+++ packages/filen-mobile/src/components/view.tsx
@@ -0,0 +1,6 @@
+import { NativeView } from "react-native-boost/runtime"
+import { withUniwind } from "uniwind"
+
+export const View = withUniwind(NativeView)
+
+export default View
